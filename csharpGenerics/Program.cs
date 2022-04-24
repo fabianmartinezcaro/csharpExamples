8fe3b530a561be3c46f8f00bbcd1750a13d1a0f0
Fabian <contacto.fabianmartinezcaro@gmail.com>
2022-04-24 19:16:06 -0300

    fixing

--- csharpGenerics/Program.cs
+++ csharpGenerics/Program.cs
@@ -12,6 +12,13 @@
             electricians.AddEmployee(new Electrician(2500));
             System.Console.WriteLine(electricians.GetEmployee(0));
 
+            EmployeeStorage<Manager> manager = new EmployeeStorage<Manager>(3);
+            manager.AddEmployee(new Manager(3500));
+            manager.AddEmployee(new Manager(2500));
+            manager.AddEmployee(new Manager(5500));
+
+            System.Console.WriteLine(manager.GetEmployee(2));
+
         }
 
     }
