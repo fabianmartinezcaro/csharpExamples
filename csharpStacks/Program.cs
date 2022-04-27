7d40f5688a60839b03fc3370655349287a758b82
Fabian <contacto.fabianmartinezcaro@gmail.com>
2022-04-27 14:22:41 -0300

    csharp Stacks

---
+++ csharpStacks/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace csharpStacks{
+    
+    class Program{
+
+        public static void Main(string[] args)
+        {
+            
+            
+            Stack<string> elements = new Stack<string>();
+            
+            //ADDING ELEMENTS INTO ELEMENTS STACK
+            foreach(string element in new string[]{"Black", "White", "Green", "Red", "Blue"}){
+
+                elements.Push(element);
+
+            }
+
+
+            //PRINTING ELEMENTS VALUES
+            foreach(string element in elements){
+
+                System.Console.WriteLine(element);
+
+            }
+
+
+            //DELETING AN ELEMENTS VALUE
+            elements.Pop();
+
+            System.Console.WriteLine("NEW LIST OF ELEMENTS: ");
+            foreach(string element in elements){
+
+                System.Console.WriteLine(element);
+
+            }
+
+
+        }
+
+    }
+
+}
