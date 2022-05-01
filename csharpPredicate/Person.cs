51c251552415b37795c868d3d9115a226ac02ee7
Fabian <contacto.fabianmartinezcaro@gmail.com>
2022-05-01 18:07:09 -0300

    ERRORES POR COREGIR EN Delegate/Predicate

---
+++ csharpPredicate/Person.cs
@@ -0,0 +1,11 @@
+namespace csharpPredicate
+{
+    public class Person
+    {
+        
+        public string name {get; set;}
+        public int age {get; set;}
+
+
+    }
+}
---
+++ csharpPredicate/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace csharpPredicate
+{
+    
+    class Program{
+
+        public static void Main(string[] args){
+            
+            List<Person> people = new List<Person>();
+
+            Person JohnExist = new Person();
+            JohnExist.name = "John";
+            JohnExist.age = 18;
+
+            Person TimExist = new Person();
+            TimExist.name = "Tim";
+
+            Person GeorgeExist = new Person();
+            GeorgeExist.name = "George";
+
+            people.AddRange(new Person[]{JohnExist, TimExist, GeorgeExist});
+
+            Predicate<Person> person = new Predicate<Person>(JohnExist);
+
+            bool personExist = people.Exists(person);
+
+
+        }
+
+    }
+
+}
